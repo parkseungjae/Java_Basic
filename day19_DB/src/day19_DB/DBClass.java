662b4a26250c9c3d57d1ddf3b09a5e755a6b974d
parkseungjae <tmd0915mp@naver.com>
2021-08-27 12:55:21 +0900

    점심시간후진행

--- day19_DB/src/day19_DB/DBClass.java
+++ day19_DB/src/day19_DB/DBClass.java
@@ -1,10 +1,24 @@
 package day19_DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class DBClass {
+	
+	private String url;
+	private String id;
+	private String pwd;
+	private Connection con;
+	
 	public DBClass() {
 		try {
 			//자바에서 오라클에 연결할 수 있게끔 도와주는 라이브러리 등록.
-		Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			url =  "jdbc:oracle:thin:@localhost:1521:xe";
+			id = "tmd0915mp";
+			pwd = "0915";
+			con = DriverManager.getConnection(url,id,pwd);
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
